ee57f764eb79fdc42d67228b387d87d185b212b3
Noah <noah@noahveltman.com>
2014-10-21 12:28:34 -0400

    Added geocoder, spherical math notes

--- borderline.js
+++ borderline.js
@@ -1,4 +1,4 @@
-function Boundless(collection) {
+function Boundless(collection,key) {
 
   if (collection.type) {
 
@@ -8,33 +8,48 @@ function Boundless(collection) {
 
     } else if (collection.type === "Topology") {
 
+
       if (typeof topojson !== "undefined" && topojson.feature) {
 
-        var key;
+        if (!collection.objects) {
 
-        for (var i in collection.objects) {
-          key = i;
-          break;
+          //throw error
+          throw new Error("Invalid TopoJSON.");
+
         }
 
-        if (key) {
+        if (typeof key !== "string") {
 
-          var converted = topojson.feature(collection,collection.objects[key]);
+          var keys = [];
 
-          //In case it's one feature
-          if (converted.type === "Feature") {
+          for (var k in collection.objects) {
 
-            this.features = [converted];
-
-          } else {
-
-            this.features = converted.features;
+            keys.push(k);
 
           }
 
+          if (keys.length == 1) {
+
+            key = keys[0];
+
+          } else if (keys.length > 1) {
+
+            throw new Error("You supplied a topology with multiple objects: "+JSON.stringify(keys)+".  You need to specify which object to search.");
+
+          }
+
+        }
+
+        var converted = topojson.feature(collection,collection.objects[key]);
+
+        //In case it's one feature
+        if (converted.type === "Feature") {
+
+          this.features = [converted];
+
         } else {
 
-          throw new Error("Invalid TopoJSON topology received.");
+          this.features = converted.features;
 
         }
 
@@ -59,13 +74,70 @@ function Boundless(collection) {
   var that = this;
 
   this.features = this.features.map(function(f){
-    f.bbox = f.bbox || that.bounds(f);
+    f.bbox = f.bbox || that._bounds(f);
     return f;
   });
 
   return this;
 
 }
+
+Boundless.prototype.findAddress = function(address,cb) {
+
+  //throw an error if google not available
+  if (!this.geocoder) {
+    try {
+      this.geocoder = new google.maps.Geocoder();
+    } catch (e) {
+      throw new Error("Couldn't initialize Google geocoder. Make sure you've included the Google Maps API too (https://developers.google.com/maps/documentation/javascript/).");
+    }
+  }
+
+  var search = {
+    "address": address
+  };
+
+  //if (Array.isArray(this.searchBounds)) {
+  //  search.bounds = this.searchBounds = new google.maps.LatLngBounds(
+  //      new google.maps.LatLng(Math.min(this.bounds[0][1],this.bounds[1][1]),Math.min(this.bounds[0][0],this.bounds[1][0])),
+  //      new google.maps.LatLng(Math.max(this.bounds[0][1],this.bounds[1][1]),Math.max(this.bounds[0][0],this.bounds[1][0]))
+  //  );
+  //}
+
+  var that = this;
+
+
+  this.geocoder.geocode(search,function(results, status) {
+
+    if (status != google.maps.GeocoderStatus.OK) {
+      return cb(status,null);
+    }
+
+    if (search.bounds) {
+      results = results.filter(function(d){
+        //fix this
+        return true;
+      });
+    }
+
+    if (!results.length) {
+      return cb("No location found.",null);
+    }
+
+    var ll = {
+      lng: results[0].geometry.location.lng(),
+      lat: results[0].geometry.location.lat()
+    };
+
+    cb(null,{
+      lat: ll.lat,
+      lng: ll.lng,
+      result: that.find([ll.lng,ll.lat])
+    });
+
+  });
+
+};
 
 Boundless.prototype.find = function(point) {
 
@@ -78,18 +150,18 @@ Boundless.prototype.find = function(point) {
   }
   return null;
 
-}
+};
 
 Boundless.prototype.inside = function(point,feature) {
 
-    if (point[0] < feature.bbox[0][0] || point[0] > feature.bbox[1][0] || point[1] < feature.bbox[0][1] || point[1] > feature.bbox[1][1]) {
+    if (feature.bbox && !this._inBox(point,feature.bbox)) {
       return false;
     }
 
     var that = this;
 
     var inRing = function(ring){
-        return that.pip(point,ring);
+        return that._pip(point,ring);
     };
 
     if (feature.geometry.type === "Polygon") {
@@ -104,9 +176,9 @@ Boundless.prototype.inside = function(point,feature) {
 
     return false;
 
-}
+};
 
-Boundless.prototype.pip = function(point, vs) {
+Boundless.prototype._pip = function(point, vs) {
 
   // ray-casting algorithm based on
   // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
@@ -120,16 +192,21 @@ Boundless.prototype.pip = function(point, vs) {
       var xi = vs[i][0], yi = vs[i][1];
       var xj = vs[j][0], yj = vs[j][1];
       
-      var intersect = ((yi > y) != (yj > y))
-          && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+      var intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
       if (intersect) inside = !inside;
   }
   
   return inside;
 
-}
+};
 
-Boundless.prototype.bounds = function(feature) {
+Boundless.prototype._inBox = function(point,box) {
+  //This doesn't work for features that cross 180 degrees longitude (e.g. Alaska)
+  //TODO: Make this work for spherical math
+  return point[0] >= box[0][0] && point[0] <= box[1][0] && point[1] >= box[0][1] && point[1] <= box[1][1];
+};
+
+Boundless.prototype._bounds = function(feature) {
 
   //Don't check inner rings
   var outer = feature.geometry.type === "Polygon" ? [feature.geometry.coordinates[0]] : feature.geometry.coordinates.map(function(f){
@@ -137,7 +214,7 @@ Boundless.prototype.bounds = function(feature) {
   });
 
   //For each point, extend bounds as needed
-  var bounds = [[Infinity,Infinity],[-Infinity,-Infinity]]
+  var bounds = [[Infinity,Infinity],[-Infinity,-Infinity]];
 
   outer.forEach(function(polygon){
 
@@ -145,12 +222,12 @@ Boundless.prototype.bounds = function(feature) {
 
       bounds = [
         [
-          Math.min(point[0][0],bounds[0][0]),
-          Math.min(point[0][1],bounds[0][1])
+          Math.min(point[0],bounds[0][0]),
+          Math.min(point[1],bounds[0][1])
         ],
         [
-          Math.max(point[1][0],bounds[1][0]),
-          Math.max(point[1][1],bounds[1][1])
+          Math.max(point[0],bounds[1][0]),
+          Math.max(point[1],bounds[1][1])
         ]
       ];
 
