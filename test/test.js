ded028c29f7af171976d9f5ac77565ef7d262cb3
Noah <noah@noahveltman.com>
2014-11-13 17:20:36 -0500

    Initial test file

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var wherewolf = require("wherewolf"),
+    assert = require("assert");
+
+/*
+assert.deepEqual(actual, expected, [message])
+assert.strictEqual(actual, expected, [message])
+assert.throws(
+  function() {
+    throw new Error("Wrong value");
+  },
+  Error
+);*/
